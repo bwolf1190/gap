80f50ea417b7e902e99318f854cea99fa9d14bb4
Zen Master Brett <bwolverton@greatamericanpower.com>
2016-12-13 13:32:41 -0600

    Added customer/plan info to enrollments table in admin portal. Removed glyphicons from admin dashboard view.

--- resources/views/enrollments/table.blade.php
+++ resources/views/enrollments/table.blade.php
@@ -5,7 +5,7 @@
             <th><a class="sort-link"onclick="sort(this);" href="/sort-enrollments/enroll_date">Enroll Date</a></th>
 			<th><a class="sort-link" href="/sort-enrollments/confirm_date">Confirm Date</a></th>
             <th><a class="sort-link" href="/sort-enrollments/agent_id">Agent Id</a></th>
-            <th><a class="sort-link" href="/sort-enrollments/customer_id">Customer Id</a></th>
+            <th><a class="sort-link" href="/sort-enrollments/customer_id">Customer</a></th>
             <th><a class="sort-link" href="/sort-enrollments/plan_id">Plan Id</a></th>
 			<th><a class="sort-link" href="/sort-enrollments/status">Status</a></th>
     <th width="50px">Action</th>
@@ -17,8 +17,8 @@
             <td>{!! $enrollment->enroll_date !!}</td>
 			<td>{!! $enrollment->confirm_date !!}</td>
             <td>{!! $enrollment->agent_id !!}</td>
-            <td>{!! Html::linkAction('CustomerController@show', $enrollment->customer_id, [$enrollment->customer_id]) !!}</td>
-            <td>{!! Html::linkAction('PlanController@show', $enrollment->plan_id, [$enrollment->plan_id]) !!}</td>
+            <td>{!! $enrollment->customer->fname . " " . $enrollment->customer->lname !!}</td>
+            <td>{!! $enrollment->plan->code !!}</td>
 			<td>{!! $enrollment->status !!}</td>
             <td>
                 <a href="{!! route('enrollments.edit', [$enrollment->id]) !!}"><i class="glyphicon glyphicon-edit"></i></a>
