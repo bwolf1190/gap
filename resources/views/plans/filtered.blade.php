f839138b0e927783abe8cd08535324f283f5e0ee
Zen Master Brett <bwolverton@greatamericanpower.com>
2017-02-06 14:09:45 -0600

    Changed rewards link to red and bold. Changed popover to allow for <b> tags around AND. Added attribute to rewards popover so that it is not overlapped by plans on the next row.

--- resources/views/plans/filtered.blade.php
+++ resources/views/plans/filtered.blade.php
@@ -29,9 +29,9 @@
                       @if($plan->meter != "")
                         <div class="price_table_row"><strong>{!! $plan->meter . " Meter" !!}</strong></div>
                       @endif
-                      <div class="price_table_row">
+                      <div class="price_table_row reward">
                         <a href="{{ $plan->reward_link }}" title="Click for more information" target="_blank">{!! $plan->reward !!}</a>
-                        <a href="#" id="acc-num-tooltip" data-toggle="popover" data-content="{{ $plan->reward_description }}" data-placement="bottom"><span class="glyphicon glyphicon-question-sign"></span></a>
+                        <a href="#" id="acc-num-tooltip" data-container="body" data-html="true" data-toggle="popover" data-content="{{ $plan->reward_description }}" data-placement="bottom"><span class="glyphicon glyphicon-question-sign"></span></a>
                       </div>
                       <div class="price_table_row etf last_row">
                           <strong>{!! $plan->etf !!}</strong>
