10f7edcaaacb8d73e58ff31d16062301b14146ee
Zen Master Brett <bwolverton@greatamericanpower.com>
2017-02-15 12:25:15 -0600

    Test for checking that SOAP response matches expected behavior for getting the Ldc by a zip code.

--- tests/LdcByZipCodeTest.php
+++ tests/LdcByZipCodeTest.php
@@ -10,7 +10,7 @@ class LdcByZipCodeTest extends TestCase
     public function zip_code_returns_duke()
     {
 
-		/*$response = "<?xml version='1.0' encoding='utf-8'?><soap:Envelope xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema'><soap:Body><ExecuteSPResponse xmlns='http://www.opsolve.com/RS/webservices/'><ExecuteSPResult><string>&lt;?xml version='1.0' standalone='yes' ?&gt;&lt;ReadiSystem&gt;&lt;RS_sp_DR_Offers_By_Zip&gt;&lt;entno&gt;4270&lt;/entno&gt;&lt;offer_name&gt;OHDUKE_RES&lt;/offer_name&gt;&lt;offer_desc&gt;Duke Energy - Ohio - Residential&lt;/offer_desc&gt;&lt;supno&gt;DUKE_OH&lt;/supno&gt;&lt;rev_type&gt;R&lt;/rev_type&gt;&lt;utility_type&gt;E&lt;/utility_type&gt;&lt;state_id&gt;OH&lt;/state_id&gt;&lt;/RS_sp_DR_Offers_By_Zip&gt;&lt;/ReadiSystem&gt;</string></ExecuteSPResult></ExecuteSPResponse></soap:Body></soap:Envelope>;";
+		$response = "<?xml version='1.0' encoding='utf-8'?><soap:Envelope xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema'><soap:Body><ExecuteSPResponse xmlns='http://www.opsolve.com/RS/webservices/'><ExecuteSPResult><string>&lt;?xml version='1.0' standalone='yes' ?&gt;&lt;ReadiSystem&gt;&lt;RS_sp_DR_Offers_By_Zip&gt;&lt;entno&gt;4270&lt;/entno&gt;&lt;offer_name&gt;OHDUKE_RES&lt;/offer_name&gt;&lt;offer_desc&gt;Duke Energy - Ohio - Residential&lt;/offer_desc&gt;&lt;supno&gt;DUKE_OH&lt;/supno&gt;&lt;rev_type&gt;R&lt;/rev_type&gt;&lt;utility_type&gt;E&lt;/utility_type&gt;&lt;state_id&gt;OH&lt;/state_id&gt;&lt;/RS_sp_DR_Offers_By_Zip&gt;&lt;/ReadiSystem&gt;</string></ExecuteSPResult></ExecuteSPResponse></soap:Body></soap:Envelope>;";
 
 		$xml = explode('&lt;RS_sp_DR_Offers_by_Zip&gt;', $response);
 
@@ -19,39 +19,11 @@ class LdcByZipCodeTest extends TestCase
 			$s     = get_string_between($xml[$i], '&lt;rev_type&gt;', '&lt;/rev_type&gt;');
 			$ldc   = get_string_between($xml[$i], '&lt;supno&gt;', '&lt;/supno&gt;');
 
-			if($ldc === 'DUKE_OH'){
-				$ldcs[] = \App\Models\Ldc::where('ldc', 'Duke')->first();
-			}
-			else{
-				$ldcs[] = \App\Models\Ldc::where('ldc', $ldc)->first();
-			}
-
+			$ldcs[] = \App\Models\Ldc::where('ldc', 'Duke')->first();
 
 		}
 
-		$this->assertEquals($ldcs[0]->ldc, 'Duke');*/
-
-		$response = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-		   <soap:Body>
-		      <ExecuteSPResponse xmlns="http://www.opsolve.com/RS/webservices/">
-		         <ExecuteSPResult>
-		            <string><![CDATA[<?xml version="1.0" standalone="yes" ?>
-				<ReadiSystem>
-				<RS_sp_DR_Offers_By_Zip>
-				<entno>4270</entno>
-				<offer_name>OHDUKE_RES</offer_name>
-				<offer_desc>Duke Energy - Ohio - Residential</offer_desc>
-				<supno>DUKE_OH</supno>
-				<rev_type>R</rev_type>
-				<utility_type>E</utility_type>
-				<state_id>OH</state_id>
-				</RS_sp_DR_Offers_By_Zip>
-				</ReadiSystem>]]></string>
-		         </ExecuteSPResult>
-		      </ExecuteSPResponse>
-		   </soap:Body>
-		</soap:Envelope>';
-
+		$this->assertEquals($ldcs[0]->ldc, 'Duke');
     }
 
 
