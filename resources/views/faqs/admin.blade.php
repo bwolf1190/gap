2895966e3d1b73450746cc3b19957bf0b70d5e9b
Zen Master Brett <bwolverton@greatamericanpower.com>
2017-07-11 11:15:18 -0500

    Removed title section from faqs.admin view.

--- resources/views/faqs/admin.blade.php
+++ resources/views/faqs/admin.blade.php
@@ -1,9 +1,5 @@
 @extends('admin.admin-new2')
 
-@section('navbar-brand')
-    <a id="nav-brand" class="nav-brand" href="/"> {!! Html::image('images/gap-fcp-swoosh.jpg') !!}</a>
-@endsection
-
 @section('content')
     <div class="container">
 
