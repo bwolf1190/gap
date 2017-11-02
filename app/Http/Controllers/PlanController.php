82a169d7812726b3e8cfcef3600023d965948927
Zen Master Brett <bwolverton@greatamericanpower.com>
2017-11-02 10:00:50 -0500

    PlanController commit with everything working except for pulling in multiple plans for one ldc/service combo.

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -264,61 +264,77 @@ class PlanController extends Controller
 	    
 		$response = $client->__getLastResponse(); 
 		$request = $client->__getLastRequest();
+
+		// no plans for this ldc/service combo. create plans array with empty values
+		if($request = '<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://www.opsolve.com/RS/webservices/"><SOAP-ENV:Body><ns1:ExecuteSP><ns1:user>gp_WebSales</ns1:user><ns1:password>G@Pa$$99!!</ns1:password><ns1:spName>RS_sp_EAI_Output</ns1:spName><ns1:paramList><ns1:string>@input_xml;&lt;ReadiSystem&gt;&lt;proc_type&gt;GU_sp_DR_Price_Quote&lt;/proc_type&gt;&lt;entno&gt;4270&lt;/entno&gt;&lt;supno&gt;DQE&lt;/supno&gt;&lt;rev_type&gt;C&lt;/rev_type&gt;&lt;campaign_code&gt;WEB&lt;/campaign_code&gt;&lt;request_date&gt;2017-10-31&lt;/request_date&gt;&lt;/ReadiSystem&gt;</ns1:string></ns1:paramList><ns1:outputParamList><ns1:string>output_xml;8000</ns1:string></ns1:outputParamList><ns1:langCode>en-us</ns1:langCode><ns1:entity>4270</ns1:entity></ns1:ExecuteSP></SOAP-ENV:Body></SOAP-ENV:Envelope>\n'){
+			$plans = array('priority' => '', 'name' => '', 'ldc' => '', 'type' => '', 'length' => '', 'rate' => '', 'reward' => '', 'reward_link'  => '', 'reward_description' => '', 'etf' => '', 'etf_description' => '', 'price_code' => '');
+		}
 	
 		// explode string into array of plans
 		$xml = explode('&lt;GU_sp_DR_Price_Quote&gt;', $response);
 		// delete first element that is not a plan
 		//unset($xml[0]);
-
+		//dd(count($xml));
+		//$plans = array();
 		// get values from xml tags and add to array
-		for($i = 1; $i < count($xml); $i++){
-			$entno = get_string_between($xml[$i], '&lt;entno&gt;', '&lt;/entno&gt;');
-			$supno= get_string_between($xml[$i], '&lt;supno&gt;', '&lt;/supno&gt;');
-			$price_code = get_string_between($xml[$i], '&lt;price_code&gt;', '&lt;/price_code&gt;');
-			$rev_type = get_string_between($xml[$i], '&lt;rev_type&gt;', '&lt;/rev_type&gt;');
-			$offer_price = get_string_between($xml[$i], '&lt;offer_price&gt;', '&lt;/offer_price&gt;');
-			$early_term_type = get_string_between($xml[$i], '&lt;early_term_type&gt;', '&lt;/early_term_type&gt;');
-			$early_term_amt = get_string_between($xml[$i], '&lt;early_term_amt&gt;', '&lt;/early_term_amt&gt;');
-			$offer_term = get_string_between($xml[$i], '&lt;offer_term&gt;', '&lt;/offer_term&gt;');
-			$price_id = get_string_between($xml[$i], '&lt;price_id&gt;', '&lt;/price_id&gt;');
-
-			if($early_term_amt == '0.00'){
-				$early_term_type = 'No Early Cancellation Fee';
-				$early_term_amt = 'No Early Cancellation Fee';
-			}
-
-			$plans[] = array('priority' => '0', 'name' => 'Fixed', 'ldc' => $supno, 'type' => $rev_type, 'length' => $offer_term, 'rate' => '$' . substr($offer_price, 0, 6) . '/kWh', 'reward' => '', 'reward_link'	 => '', 'reward_description' => '', 'etf' => $early_term_amt, 'etf_description' => $early_term_type, 'price_code' => $price_code);
-		}
-
-		foreach($plans as $plan){
-			if($plan['ldc'] === 'DELMD'){ $plan['ldc'] = 'Delmarva'; }
-
-			if($plan['ldc'] === 'DUKE_OH'){ $plan['ldc'] = 'Duke'; }
+		for($i = 0; $i < count($xml); $i++){
+			$entno = get_string_between($xml[$i], "&lt;entno&gt;", "&lt;/entno&gt;");
+			$supno= get_string_between($xml[$i], "&lt;supno&gt;", "&lt;/supno&gt;");
+			$price_code = get_string_between($xml[$i], "&lt;price_code&gt;", "&lt;/price_code&gt;");
+			$rev_type = get_string_between($xml[$i], "&lt;rev_type&gt;", "&lt;/rev_type&gt;");
+			//$price_desc = get_string_between($xml[$i], "&lt;price_desc&gt;", "&lt;/price_desc&gt;");
+			$offer_price = get_string_between($xml[$i], "&lt;offer_price&gt;", "&lt;/offer_price&gt;");
+			$early_term_type = get_string_between($xml[$i], "&lt;early_term_type&gt;", "&lt;/early_term_type&gt;");
+			$early_term_amt = get_string_between($xml[$i], "&lt;early_term_amt&gt;", "&lt;/early_term_amt&gt;");
+			$offer_term = get_string_between($xml[$i], "&lt;offer_term&gt;", "&lt;/offer_term&gt;");
+			$price_id = get_string_between($xml[$i], "&lt;price_id&gt;", "&lt;/price_id&gt;");
 			
-			if($plan['ldc'] === 'DQE'){ $plan['ldc'] = 'Duquesne'; }
-
-			if($plan['etf_description'] === 'FIXED'){
-				$plan['etf_description'] = 'Flat fee of ' . '$' . $plan['etf'] . ' for early cancellation.';
-				$plan['etf'] = 'Cancellation Fee Applies';
-			}
-			else if($plan['etf_description'] === 'TERM' || $plan['etf_description'] === 'REDUC' && $plan['etf'] !== ''){
-				$plan['etf_description'] = '$' . $plan['etf'] . ' per month remaining on the contract.';
-				$plan['etf'] = 'Cancellation Fee Applies';
-			}
-			else{
-				$plan['etf_description'] = 'No fee for terminating the contract early.';	
-			}
-			if($plan['type'] === 'R'){
-				$plan['type'] = 'Residential';
-				$plan['reward'] = '$200 Security System Rebate';
-				$plan['reward_link'] = 'http://www.safestreets.com/GAP1/';
-				$plan['reward_description'] = 'Receive a $100 rebate from Great American Power AND a $100 rebate for signing up with our Home Security partners, Safe Streets (an authorized dealer of ADT security systems)';
-			}
-			else{
-				$plan['type'] = 'Commercial';
+			if($early_term_type == 'FIXED' && $early_term_amt !== '0.00'){
+				$etf_description = 'Flat fee of $' . $early_term_amt . ' for early cancellation.';
+				$early_term_type = 'Cancellation Fee Applies'; 
 			}
 
-			$p = \App\Models\Plan::create($plan);
+			else if($early_term_type == 'REDUC' || $early_term_type == 'TERM'){
+				$etf_description = '$' . $early_term_amt . ' per month remaining on contract.';
+				$early_term_type = 'Cancellation Fee Applies'; 
+			}
+
+			else{
+				$etf_description = 'No fee for terminating the contract early.';
+				$early_term_type = 'No Early Cancellation Fee';
+			}
+
+			//$plans = array('priority' => '0', 'name' => 'Fixed', 'ldc' => $supno, 'type' => $rev_type, 'length' => $offer_term, 'rate' => '$' . substr($offer_price, 0, 6) . '/kWh', 'reward' => '', 'reward_link'	 => '', 'reward_description' => '', 'etf' => $early_term_amt, 'etf_description' => $etf_description, 'price_code' => $price_code);
+			$plans = array('priority' => '0', 'name' => 'Fixed', 'ldc' => $supno, 'type' => $rev_type, 'length' => $offer_term, 'rate' => '$' . substr($offer_price, 0, 6) . '/kWh', 'reward' => '', 'reward_link'  => '', 'reward_description' => '', 'etf' => $early_term_type, 'etf_description' => $etf_description, 'price_code' => (int)$price_code);
+			//$plans['priority' ,'name', 'ldc', 'type', 'length', 'rate', 'reward', 'reward_link', 'reward_description', 'etf', 'etf_description', 'price_code'] = ['0', 'Fixed', $supno, $rev_type, $offer_term, '$' . substr($offer_price, 0, 6) . '/kWh', '', '', '', $early_term_amt, $etf_description, (int)$price_code];
+		}
+		
+		//dd($plans);
+
+		foreach($plans as $key => $val){
+			$input[$key] = $val;
+			if($key == 'ldc' && $input[$key] == 'DUKE_OH'){
+				$input['ldc'] = 'Duke';
+			}
+			if($key == 'ldc' && $input[$key] == 'DELMD'){
+				$input['ldc'] = 'Delmarva';
+			}
+			if($key == 'ldc' && $input[$key] == 'DQE'){
+				$input['ldc'] = 'Duquesne';
+			}
+			if($key == 'ldc' && $input[$key] == 'PEPCO_MD'){
+				$input['ldc'] = 'PEPCO';
+			}
+			if($key == 'length' && $input[$key] == '1'){
+				$input['name'] = 'Variable';		
+			}
+			if($input[$key] == 'R'){ $input[$key] = 'Residential'; }
+			if($input[$key] == 'C'){ $input[$key] = 'Commercial'; }
+
+			// only add plans that have all information
+			if($key == 'price_code' && $val !== 0){
+				$p = \App\Models\Plan::create($input);
+			}
 		}
 	}
 
@@ -326,34 +342,34 @@ class PlanController extends Controller
 	/**
 	 * Delete all Opsolve plans from database
 	 * Update plans for each utility
-	 * Redirect to home page
+	 * Redirect to plans page
 	 */
 	public function updatePlans(){
 		\App\Models\Plan::truncate();
 
-		//$this->updateLdcPlans('BGE', 'R');
-		//$this->updateLdcPlans('BGE', 'C');
+		$this->updateLdcPlans('BGE', 'R');
+		$this->updateLdcPlans('BGE', 'C');
 
-		//$this->updateLdcPlans('DELMD', 'R');
-		//$this->updateLdcPlans('DELMD', 'C');
+		$this->updateLdcPlans('DELMD', 'R');
+		$this->updateLdcPlans('DELMD', 'C');
 		
 		$this->updateLdcPlans('DUKE_OH', 'R');
 		$this->updateLdcPlans('DUKE_OH', 'C');
 		
-		//$this->updateLdcPlans('DQE', 'R');
-		//$this->updateLdcPlans('DQE', 'C');
-
-		//$this->updateLdcPlans('METED', 'R');
-		//$this->updateLdcPlans('METED', 'C');
+		$this->updateLdcPlans('DQE', 'R');
+		$this->updateLdcPlans('DQE', 'C');
+ 
+		$this->updateLdcPlans('METED', 'R');
+		$this->updateLdcPlans('METED', 'C');
 		
-		//$this->updateLdcPlans('PECO', 'R');
-		//$this->updateLdcPlans('PECO', 'C');
+		$this->updateLdcPlans('PECO', 'R');
+		$this->updateLdcPlans('PECO', 'C');
 		
-		//$this->updateLdcPlans('PEPCO', 'R');
-		//$this->updateLdcPlans('PEPCO', 'C');
+		$this->updateLdcPlans('PEPCO_MD', 'R');
+		$this->updateLdcPlans('PEPCO_MD', 'C');
 
-		//$this->updateLdcPlans('PPL', 'R');
-		//$this->updateLdcPlans('PPL', 'C');
+		$this->updateLdcPlans('PPL', 'R');
+		$this->updateLdcPlans('PPL', 'C');
 
 		return redirect('/plans');
 	}
