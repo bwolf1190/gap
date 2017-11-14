7104bcbef6e85bb88dd8b737483b3b76f2c3504c
Zen Master Brett <bwolverton@greatamericanpower.com>
2017-11-14 16:08:45 -0600

    Added customers account number to the broker-confirmed email view.

--- resources/views/emails/broker-confirmed.blade.php
+++ resources/views/emails/broker-confirmed.blade.php
@@ -3,6 +3,9 @@
 		<p>
 			{!! $customer->fname . " " . $customer->lname . " has confirmed their email address.  Their enrollment will be processed within 24 hours. " !!}  
 		</p>
+		<p>
+			{!! "Account #: " . $customer->acc_num !!}
+		</p>
 	</div>
 </div>
 
